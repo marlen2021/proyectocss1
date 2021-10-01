d4e2addf4e13a35dc7e36c93c7ad415babcbd71a
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-09-30 20:47:19 -0500

    Update Cliente.php

--- backend/api/app/Models/Cliente.php
+++ backend/api/app/Models/Cliente.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+    
 
 class Cliente extends Model
 {
-    use HasFactory;
+    use Notifiable;
+    public $timestamps = false;
+    protected $table = 'clientes';
+    protected $primaryKey = 'doc';
+    protected $fillable = ['doc', 'nombre', 'apellidos', 'correo', 'clave', 'registro'];
+
+    public function reservas(){
+        return $this->hasMany(Reserva::class);
+    }
 }
