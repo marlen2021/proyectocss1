65447c908492e78013eadfd5969af017bd90b699
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-09-30 20:47:06 -0500

    Update Reserva.php

--- backend/api/app/Models/Reserva.php
+++ backend/api/app/Models/Reserva.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reserva extends Model
 {
-    use HasFactory;
+    public $timestamps = false;
+    protected $table = 'reservas';
+    protected $fillable = ['habitacion_id', 'fecha_inicio', 'fecha_fin', 'huespedes', 'doc_cliente'];
 }
