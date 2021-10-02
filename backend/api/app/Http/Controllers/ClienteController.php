3c2433d89d4f75aa8da2c478cb8f2a4449bef91e
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-10-02 00:21:10 -0500

    Update ClienteController.php

--- backend/api/app/Http/Controllers/ClienteController.php
+++ backend/api/app/Http/Controllers/ClienteController.php
@@ -7,6 +7,22 @@ use App\Models\Cliente;
 
 class ClienteController extends Controller
 {
+    public function signup(Request $request){
+        return Cliente::create($request->all());
+    }
+
+    public function login(Request $request){
+        $correo= $request->input('correo');
+        $clave= $request->input('clave');
+
+        $cliente=Cliente::select('clave')->where('correo',$correo)->first();
+        return $cliente;
+    }
+
+    public function update(Request $request){
+
+    }
+
     public function showAll(){ 
         return Cliente::All();
     }
