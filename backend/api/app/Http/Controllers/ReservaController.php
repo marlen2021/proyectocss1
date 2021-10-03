f57d633c88d3b7cb84c7c01452be1e78ba55f464
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-10-03 12:07:41 -0500

    Update ReservaController.php

--- backend/api/app/Http/Controllers/ReservaController.php
+++ backend/api/app/Http/Controllers/ReservaController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reserva;
+use JWTAuth;
+use Illuminate\Database\QueryException;
 
 class ReservaController extends Controller
 {
-    //
+    public function create(Request $request){
+        return "hola";
+    }
 }
