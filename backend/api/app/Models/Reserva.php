f943e6e5abf7c78858f3ae14415e7eba793a86af
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-10-07 15:04:42 -0500

    Update Reserva.php

--- backend/api/app/Models/Reserva.php
+++ backend/api/app/Models/Reserva.php
@@ -9,4 +9,10 @@ class Reserva extends Model
     public $timestamps = false;
     protected $table = 'reservas';
     protected $fillable = ['habitacion_id', 'fecha_inicio', 'fecha_fin', 'huespedes', 'doc_cliente'];
+    public function cliente(){
+        return $this->belongsTo(Cliente::class);
+    }
+    public function habitacion(){
+        return $this->belongsTo(Habitacion::class);
+    }
 }
