ad8171c3e14c5d4125c117018f68050406ef81ff
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-10-07 15:04:57 -0500

    Update ReservaController.php

--- backend/api/app/Http/Controllers/ReservaController.php
+++ backend/api/app/Http/Controllers/ReservaController.php
@@ -10,6 +10,22 @@ use Illuminate\Database\QueryException;
 class ReservaController extends Controller
 {
     public function create(Request $request){
-        return "hola";
+        $data = $request->all();
+        $token = $request->bearerToken();
+        $doc = JWTAuth::getPayload($token)->toArray()['sub'];
+        $data['doc_cliente'] = $doc;
+        try{
+            $created = is_object(Reserva::create($data));
+        }catch(QueryException $e){
+            $created = false;
+        }
+        return array('created' => $created);
+    }
+    public function showByCliente(Request $request){
+        $token = $request->bearerToken();
+        $doc = JWTAuth::getPayload($token)->toArray()['sub'];
+        /* $history = Reserva::where('doc_cliente', $doc)->with(['habitacion.hotel', 'habitacion.tipo'])->get(); */
+        $history = Reserva::where('doc_cliente', $doc)->get();
+        return $history;
     }
 }
