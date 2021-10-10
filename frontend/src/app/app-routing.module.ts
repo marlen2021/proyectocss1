827798a5f95ff8f891e17916e9c908079c273ca7
Jonnathan777 <90125734+Jonnathan777@users.noreply.github.com>
2021-10-10 12:47:06 -0500

    Create app-routing.module.ts

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+/* import { HomeComponent } from './components/home/home.component'; */
+import { LoginComponent } from './components/login/login.component';
+/* import { ProfileComponent } from './components/profile/profile.component'; */
+/* import { ReservationComponent } from './components/reservation/reservation.component'; */
+import { SignupComponent } from './components/signup/signup.component';
+
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+/*   { path: 'profile', component: ProfileComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'reservation', component: ReservationComponent }, */
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
